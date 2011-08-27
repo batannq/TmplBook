5f59d4658bbcfe9127a1f14c15336c2bc0e68f54
中野 俊亮 <xerophyte@imac-snakano.lan>
2011-08-27 14:46:40 +0900

    Initial Commit

---
+++ TmplBook/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TmplBook
+//
+//  Created by 中野 俊亮 on 11/08/27.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
